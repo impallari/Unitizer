57307347194a5ee6965bb7ab82a58a802b2ecab9
schriftgestalt <georg.seifert@mac.com>
2021-06-26 18:49:17 +0200

    her better API

--- Unitizer.glyphsReporter/Contents/Resources/plugin.py
+++ Unitizer.glyphsReporter/Contents/Resources/plugin.py
@@ -25,15 +25,17 @@ class Unitizer (ReporterPlugin):
 			yBottom = -200.0 # default descender
 			yTop    =  800.0 # default ascender
 			unit    =  40.0  # default unit
-			
+			slant = None
+			center = 0
 			try: # try to overwrite defaults with actual values from the master:
 				currentMaster = Layer.associatedFontMaster()
 				yBottom = currentMaster.descender
 				yTop    = currentMaster.ascender
-				slant   = currentMaster.italicAngle / 180 * pi
-				displacement = tan(slant) * (yTop-yBottom)
-				# See https://github.com/impallari/Unitizer/issues/4 for explanation
-				xDisplacement = (-yBottom + 0.5 * currentMaster.xHeight) * tan(slant)
+				
+				italicAngle = currentMaster.italicAngleForLayer_(Layer)
+				if abs(italicAngle) > 0.001:
+					slant = italicAngle
+					center = currentMaster.slantHeightForLayer_(Layer)
 				customParameter = currentMaster.customParameters['unitizerUnit']
 				if customParameter:
 					unit = float(customParameter)
@@ -42,33 +44,34 @@ class Unitizer (ReporterPlugin):
 					self.hasWarned = True
 			except Exception as e:
 				print(traceback.format_exc())
-			
 			# color the width gap:
 			NSColor.redColor().set() # set color to red
 			gap = layerWidth % unit
-			gapStart = layerWidth-gap - xDisplacement
+			gapStart = layerWidth - gap
 			if gap:
 				bl = NSPoint( gapStart, yBottom )
-				tl = NSPoint( gapStart + displacement, yTop )
+				tl = NSPoint( gapStart, yTop )
 				br = NSPoint( gapStart + gap, yBottom)
-				tr = NSPoint( gapStart + gap + displacement, yTop )
+				tr = NSPoint( gapStart + gap, yTop )
 				gapRectangle = NSBezierPath.alloc().init()
 				gapRectangle.moveToPoint_(bl)
 				gapRectangle.lineToPoint_(tl)
 				gapRectangle.lineToPoint_(tr)
 				gapRectangle.lineToPoint_(br)
 				gapRectangle.lineToPoint_(bl)
+				if slant is not None:
+					gapRectangle.transformWithAngle_center_(slant, center)
 				gapRectangle.fill()
 
 			# draw vertical lines:
 			# starting point = 1 unit away from LSB at the descender line.
-			x = unit - xDisplacement
+			x = unit
 			unitLines = NSBezierPath.alloc().init() # initialize a path object myPath
 			# while x < gapStart:
 			while x <= gapStart:
 				# draw vertical line:
 				unitLines.moveToPoint_( NSPoint( x, yBottom ) )
-				unitLines.lineToPoint_( NSPoint( x+displacement, yTop    ) )
+				unitLines.lineToPoint_( NSPoint( x, yTop    ) )
 				x += unit # advance to the right
 			if gap:
 				# set color for unit lines:
@@ -80,6 +83,8 @@ class Unitizer (ReporterPlugin):
 
 			# unitLines.setLineWidth_( 1.0 / self.getScale() ) # set stroke thickness for current scale
 			unitLines.setLineWidth_( 1.0 ) # Always 1
+			if slant is not None:
+				unitLines.transformWithAngle_center_(slant, center)
 			unitLines.stroke() # add stroke to the path object
 			
 		except Exception as e:
